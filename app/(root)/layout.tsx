9762c3290ec71d525a88458fdad3de18b0d8f33f
Danish Firhan <danishfirhan@graduate.utm.my>
2024-08-29 00:43:03 +0800

    Added header and footer

---
+++ app/(root)/layout.tsx
@@ -0,0 +1,20 @@
+import Footer from '@/components/shared/footer'
+import Header from '@/components/shared/header'
+import React from 'react'
+
+export default function RootLayout({
+    children,
+    modal,
+    }: {
+    children: React.ReactNode
+    modal: React.ReactNode
+    }) {
+    return (
+    <div className="flex h-screen flex-col">
+        <Header />
+        <main className="flex-1 wrapper">{children}</main>
+        {modal}
+        <Footer />
+    </div>
+    )
+}
---
+++ lib/constants/index.ts
@@ -0,0 +1,59 @@
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'MusicRecords2U'
+export const APP_DESCRIPTION =
+process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
+'An e-commerce vinyl records store built with Next.js, Postgres, Shadcn'
+
+export const SENDER_EMAIL = process.env.SENDER_EMAIL || 'onboarding@resend.dev'
+
+export const PAGE_SIZE = Number(process.env.PAGE_SIZE) || 3
+
+export const PAYMENT_METHODS = process.env.PAYMENT_METHODS
+? process.env.PAYMENT_METHODS.split(', ')
+: ['PayPal', 'Stripe', 'CashOnDelivery']
+export const DEFAULT_PAYMENT_METHOD =
+process.env.DEFAULT_PAYMENT_METHOD || 'PayPal'
+
+export const USER_ROLES = process.env.USER_ROLES
+? process.env.USER_ROLES.split(', ')
+: ['admin', 'user']
+
+export const signInDefaultValues = {
+email: '',
+password: '',
+}
+
+export const signUpDefaultValues = {
+name: '',
+email: '',
+password: '',
+confirmPassword: '',
+}
+
+export const shippingAddressDefaultValues = {
+fullName: '',
+streetAddress: '',
+city: '',
+postalCode: '',
+country: '',
+}
+
+export const productDefaultValues = {
+name: '',
+slug: '',
+category: '',
+images: [],
+brand: '',
+description: '',
+price: '0',
+stock: 0,
+rating: '0',
+numReviews: 0,
+isFeatured: false,
+banner: null,
+}
+
+export const reviewFormDefaultValues = {
+title: '',
+comment: '',
+rating: 0,
+}
