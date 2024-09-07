cf870c375f6573db7beabba13ad46d37765d567e
Danish Firhan <danishfirhan@graduate.utm.my>
2024-09-07 13:28:50 +0800

    Improved Logo Font

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -1,28 +1,27 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import { APP_NAME } from '@/lib/constants'
-import Menu from './menu'
-
+import Image from 'next/image';
+import Link from 'next/link';
+import { APP_NAME } from '@/lib/constants';
+import Menu from './menu';
 
 const Header = async () => {
 return (
-    <header className="w-full border-b">
+<header className="w-full border-b">
     <div className="wrapper flex-between">
     <div className="flex-start">
-        <Link href="/" className="flex-start">
+        <Link href="/" className="flex items-center space-x-2">
         <Image
             src="/assets/icons/logo.svg"
             width={48}
             height={48}
             alt={`${APP_NAME} logo`}
         />
-        {APP_NAME}
+        <span className="font-bold text-xl">{APP_NAME}</span>
         </Link>
     </div>
-    <Menu/>
-        </div>
+    <Menu />
+    </div>
 </header>
-)
-}
+);
+};
 
-export default Header
+export default Header;
--- components/shared/header/menu.tsx
+++ components/shared/header/menu.tsx
@@ -6,10 +6,10 @@ const Menu = () => {
     return (
         <div className="flex justify-end gap-3">
             <nav className="md:flex hidden w-full max-w-xs gap-1">
-                <Link href="/about-us" className="p-2">
+            <Link href="/about-us" className="p-2 whitespace-nowrap">
                     About Us
                 </Link>
-                <Link href="/contact-us" className="p-2">
+                <Link href="/contact-us" className="p-2 whitespace-nowrap">
                     Contact Us
                 </Link>
                 <CartButton/>
