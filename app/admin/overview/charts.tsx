d8ffb3482e5826c6345e68c4042ae24624e7a7e9
Danish Firhan <danishfirhan@graduate.utm.my>
2024-09-11 03:21:20 +0800

    fix admin dashboard bug

--- app/admin/overview/charts.tsx
+++ app/admin/overview/charts.tsx
@@ -11,7 +11,7 @@ return (
 <ResponsiveContainer width="100%" height={350}>
     <BarChart data={salesData}>
     <XAxis
-        dataKey="_id"
+        dataKey="months"
         stroke="#888888"
         fontSize={12}
         tickLine={false}
