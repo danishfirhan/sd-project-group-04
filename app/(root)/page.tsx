7d103f50056e684bbc165e6f1031585658509381
Danish Firhan <danishfirhan@graduate.utm.my>
2024-09-11 13:34:29 +0800

    Added E-Commerce features and Sale Promotion

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,7 @@
+import EcommerceFeatures from '@/components/shared/product/ecommerce-features'
 import ProductCarousel from '@/components/shared/product/product-carousel'
 import ProductList from '@/components/shared/product/product-list'
+import ProductPromotion from '@/components/shared/product/product-promotion'
 import { getFeaturedProducts, getLatestProducts } from '@/lib/actions/product.actions'
 import { APP_DESCRIPTION, APP_NAME } from '@/lib/constants'
 import { Metadata } from 'next'
@@ -19,6 +21,8 @@ export default async function Home() {
       )}
       <div className="space-y-8">
         <ProductList title="Newest Arrivals" data={latestProducts} />
+        <ProductPromotion />
+        <EcommerceFeatures />
       </div>
       </div>
   )
