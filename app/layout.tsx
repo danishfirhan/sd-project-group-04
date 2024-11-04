00eb38a1c6ce10f44b9e0a57b2d8fd77842bab32
Danish Firhan <danishfirhan@graduate.utm.my>
2024-11-04 10:03:34 +0800

    Fix font and ui build errors

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,10 @@
 import type { Metadata } from 'next';
 import './globals.css';
-import { Poppins as FontSans } from 'next/font/google';
 import { cn } from '@/lib/utils';
 import { Toaster } from '@/components/ui/toaster';
 import React from 'react';
 import { ThemeProvider } from '@/components/shared/theme-provider';
 import { APP_DESCRIPTION, APP_NAME } from '@/lib/constants';
-
-const fontSans = FontSans({
-  subsets: ['latin'],
-  weight: ['400', '600'],
-  variable: '--font-sans',
-});
 
 export const metadata: Metadata = {
   title: `${APP_NAME} - ${APP_DESCRIPTION}`,
@@ -29,14 +22,10 @@ export default function RootLayout({
         <link rel="icon" href="/favicon.ico" type="image/x-icon" />
         <link rel="icon" href="/favicon-32x32.png" sizes="32x32" />
         <link rel="icon" href="/favicon-16x16.png" sizes="16x16" />
-        {/* Optionally, add other metadata like theme-color */}
         <meta name="theme-color" content="#ffffff" />
       </head>
       <body
-        className={cn(
-          'min-h-screen bg-background font-sans antialiased',
-          fontSans.variable
-        )}
+        className={cn('min-h-screen bg-background font-sans antialiased')}
       >
         <ThemeProvider
           attribute="class"
